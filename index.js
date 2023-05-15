5d91b5c0508da4ac4241606f2d262d660107d20a
Lingabathula Sharath <sharathlingabathula@gmail.com>
2023-05-15 17:00:27 +0530

    Basic socket setup done

---
+++ index.js
@@ -0,0 +1,27 @@
+const { Socket } = require('dgram');
+const express = require('express')
+const http = require('http');
+const socketio = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+io.on('connection', (Socket) => {
+    console.log('a user connected', Socket.id);
+
+    Socket.on('from_client', () => {
+        console.log('event coming from client');
+    })
+
+    setInterval(() =>{
+        Socket.emit('from_server');
+    }, 2000)
+});
+
+app.use('/', express.static(__dirname + '/public'));
+
+server.listen(3000, () => {
+    console.log('Server started');
+
+})
